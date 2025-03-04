1ef1adc4e2ee641492742351ecaa6b3f629f9ea1
Sergey Kiremshin <barenhohle@gmail.com>
2025-03-05 00:29:34 +0300

    feat: добавлен вывод карточек на страницу из имеющегося массива

--- scripts/index.js
+++ scripts/index.js
@@ -7,3 +7,25 @@
 // @todo: Функция удаления карточки
 
 // @todo: Вывести карточки на страницу
+
+
+const cardContainer = document.querySelector('.places__list');
+
+function addCard({name, link}, removeCard) {
+    const cardTemplate = document.querySelector('#card-template').content;
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    const removeButton = cardElement.querySelector('.card__delete-button');
+    cardElement.querySelector('.card__title').textContent = name;
+    cardElement.querySelector('.card__image').src = link;
+    cardElement.querySelector('.card__image').alt = 'Фотография на тему: ' + name;
+
+    removeButton.addEventListener('click', () => removeCard(cardElement));
+
+    return cardElement;
+};
+
+function removeCard(cardElement){
+    cardElement.remove();
+};
+
+initialCards.map(item => addCard(item, removeCard)).forEach((item) => cardContainer.append(item));
